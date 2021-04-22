c6f8b214c1e3cbc120096b69f6fadc44ab882612
ariaghora <hello@ghora.net>
2021-04-22 13:14:43 +0700

    add setup tool and readme

---
+++ README.md
@@ -0,0 +1,3 @@
+This is an unofficial implementation of "Fast RobustSTL: Efficient and Robust Seasonal-Trend Decomposition for Time Series with Complex Patterns"
+
+`pip install --upgrade git+https://github.com/ariaghora/mpstemmer.git`
---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='fast-robust-stl',
+    version='0.1.0',
+    description=('Unofficial implementation of "Fast RobustSTL: Efficient and Robust Seasonal-Trend Decomposition for Time Series with Complex Patterns"'),
+    author='',
+    author_email='',
+    url='https://github.com/ariaghora/torch_kernel',
+    license='MIT',
+    packages=['fast-robust-stl'],
+    include_package_data=True,
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python :: 3.8'],
+    )
